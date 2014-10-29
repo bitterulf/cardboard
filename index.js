d940507b94f4b72429f7381075359015f9e03000
unknown <KannegiesserS@LE-MSG-I232319.drefa.int>
2014-10-29 09:34:32 +0100

    Add public folder.

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server(8080);
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply('Hello, cardboard!');
+server.route([
+  {
+    method: 'GET',
+    path: '/cb',
+    handler: function (request, reply) {
+      reply('Hello, cardboard!');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+      directory: {
+        path: './public',
+        listing: true
+      }
+    }
   }
-});
+]);
 
 server.start(function () {
   console.log('Server running at:', server.info.uri);
