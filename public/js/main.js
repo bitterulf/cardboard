8dd56583477429281a0d9b8b0142bf1117925464
unknown <KannegiesserS@LE-MSG-I232319.drefa.int>
2014-10-29 22:02:07 +0100

    Add final images.

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,74 @@
 var client = new Faye.Client('http://localhost:8080/pubsub');
 
+var currentPerson = 0;
+
+var currentCategory = 0;
+
 var clips = [
   'Kai-food',
+  'Martin-work',
   'Kai-transporation',
-  'Kai-work',
-  'Malwina-food',
   'Malwina-media',
-  'Martin-media',
-  'Martin-work'
+  'Malwina-food',
+  'Kai-work',
+  'Martin-transportation',
+  'Martin-media'
 ];
 
-var markMenu = function(elem) {
+var markMenu = function(elem, id) {
+  currentCategory = id;
   $('.topmenu span').removeClass('active');
   $(elem).addClass('active');
+  updatePosition();
+};
+
+var categories = [
+  'food',
+  'work',
+  'transport',
+  'media'
+];
+
+var persons = [
+  'kai',
+  'malwina',
+  'martin'
+];
+
+var posMap = {
+  'food_kai': 1,
+  'work_martin': 2,
+  'transport_kai': 3,
+  'media_malwina': 4,
+  'food_malwina': 5,
+  'work_kai': 6,
+  'transport_martin': 7,
+  'media_martin': 8
+};
+
+var updatePosition = function() {
+  if (currentPerson && currentCategory) {
+    var key = categories[currentCategory-1]+'_'+persons[currentPerson-1];
+    if (posMap[key]) {
+      playVideo(posMap[key]);
+    }
+    else {
+      console.log('nothing found for', key);
+    }
+  }
 };
 
 var markBottomMenu = function(elem, id) {
+  currentPerson = id;
   $('.bottommenu div').removeClass('active1');
   $('.bottommenu div').removeClass('active2');
   $('.bottommenu div').removeClass('active3');
   $(elem).addClass('active'+id);
+  updatePosition();
 };
 
 var showMenu = function (id) {
+  currentCategory = id;
   $('.menus').hide();
   $('.menu' + id).show();
 };
