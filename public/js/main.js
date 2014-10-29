019e47be1438ce0193ff449e0e2a30906ab0eaca
unknown <KannegiesserS@LE-MSG-I232319.drefa.int>
2014-10-29 11:38:32 +0100

    Add scrolltto for videos.

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,37 @@
 var client = new Faye.Client('http://localhost:8080/pubsub');
 
+var playVideo = function(id) {
+  videojs(id).ready(function(){
+    var myPlayer = this;
+    myPlayer.currentTime(0);
+    myPlayer.play();
+  });
+};
+
 $(document).ready(function() {
   client.subscribe('/message', function(message) {
     console.log(message.text);
   }).then(function() {
     client.publish('/message', {text: 'example message'});
   });
-  console.log('started');
+
+  $('#button').click(function() {
+    $('#scrollcontainer').scrollTo('#scrolltarget1', 1000);
+  });
+
+  $('#button2').click(function() {
+    $('#scrollcontainer').scrollTo('#scrolltarget2', 1000);
+  });
+
+  $('#button3').click(function() {
+    $('#scrollcontainer').scrollTo('#scrolltarget3', 1000);
+    playVideo("example_video_1");
+  });
+
+  videojs("example_video_1").ready(function(){
+    var myPlayer = this;
+    myPlayer.on("ended", function(){
+      $('#scrollcontainer').scrollTo('#scrolltarget1', 1000);
+    });
+  });
 });
